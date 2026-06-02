b93c2576e7d7db792658eb5df3bd53fd3967535d
giuseppe <giuseppe@atyos.io>
2026-06-02 15:53:58 +0200

    plugin fix

--- platform/src/plugins/font-filter/code.js
+++ platform/src/plugins/font-filter/code.js
@@ -1,37 +1,26 @@
 (function(window, undefined) {
 
-    window.Asc.plugin.init = function() {};
-    window.Asc.plugin.button = function(id) {};
-
-    window.Asc.plugin.onDocumentReady = function() {
-        console.log("[FontFilter] Plugin running. Diagnosing frame hierarchy...");
+    window.Asc.plugin.init = function() {
+        console.log("[FontFilter] init called");
 
         var current = window;
         for (var depth = 0; depth < 10; depth++) {
             try {
                 var hasCommon = !!(current.Common);
                 var hasAscFonts = !!(current.AscFonts);
-                var hasNotification = !!(current.Common && current.Common.NotificationCenter);
                 var hasFontInfos = !!(current.AscFonts && current.AscFonts.g_font_infos);
                 var fontCount = hasFontInfos ? current.AscFonts.g_font_infos.length : 0;
                 var fontItems = 0;
                 try { fontItems = current.document.querySelectorAll('a.font-item').length; } catch(e) {}
-                var dropdownItems = 0;
-                try { dropdownItems = current.document.querySelectorAll('.dropdown-menu li').length; } catch(e) {}
 
                 console.log("[FontFilter] depth=" + depth +
                     " Common=" + hasCommon +
                     " AscFonts=" + hasAscFonts +
-                    " NotificationCenter=" + hasNotification +
                     " g_font_infos=" + fontCount +
-                    " fontItemsDOM=" + fontItems +
-                    " dropdownLIs=" + dropdownItems
+                    " fontItemsDOM=" + fontItems
                 );
 
-                if (current === current.parent) {
-                    console.log("[FontFilter] Reached top at depth " + depth);
-                    break;
-                }
+                if (current === current.parent) break;
                 current = current.parent;
             } catch(e) {
                 console.log("[FontFilter] depth=" + depth + " BLOCKED: " + e.message);
@@ -39,5 +28,7 @@
             }
         }
     };
+
+    window.Asc.plugin.button = function(id) {};
 
 })(window);
